98b24209cc780638305ec7d57affa59f5a43586f
David Freitas <davidfreitas.dev@outlook.com>
2023-07-18 19:52:58 -0300

    configuracoes de deploy para o GH Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sorteio-da-familia/',
   plugins: [vue()],
   resolve: {
     alias: {
