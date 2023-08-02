6f9a5c9c401b9b472e7d5cb5824957b3243105cb
David Freitas <davidfreitas@iMac-Pro-de-David.local>
2023-08-02 11:05:17 -0300

    criacao do componente de listagem de nomes

---
+++ src/components/List.vue
@@ -0,0 +1,48 @@
+<script setup>
+const props = defineProps({
+  list: {
+    type: Array,
+    default: () => ([])
+  }
+});
+</script>
+
+<template>
+  <h2 class="text-lg font-semibold text-gray-900 dark:text-white mt-5">
+    Listagem de famílias:
+  </h2>
+
+  <ul 
+    v-for="(item, index) in list"
+    :key="index"
+    class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400"
+  >
+    <li
+      class="flex items-center my-3"
+    >
+      <svg
+        v-if="item.drawn"
+        class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+      </svg>
+
+      <svg
+        v-else
+        class="w-3.5 h-3.5 mr-2 text-gray-500 dark:text-gray-400 flex-shrink-0"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+      </svg>
+      
+      {{ item.name }}
+    </li>
+  </ul>
+</template>
--- src/pages/Form.vue
+++ src/pages/Form.vue
@@ -1,21 +1,56 @@
 <script setup>
-import { ref } from 'vue';
-import { collection, addDoc } from 'firebase/firestore';
+import { onMounted, ref } from 'vue';
+import { collection, onSnapshot, addDoc } from 'firebase/firestore';
 import { db } from '@/services/firebase-firestore';
 import Text from '@/components/Text.vue';
 import TextInput from '@/components/TextInput.vue';
 import Button from '@/components/Button.vue';
+import List from '@/components/List.vue';
 
-const familyName = ref('');
+const family = ref('');
+const familyNames = ref([]);
+
+const checkExists = async (family) => {
+  return familyNames.value.includes(family.name);
+};
 
 const handleAdd = async () => {
+  const exists = await checkExists(family.value);
+
+  if (exists) {
+    console.log('Esta família já foi cadastrada');
+    return;
+  }
+
   const docRef = await addDoc(collection(db, 'families'), {
-    name: familyName.value,
+    name: family.value,
     drawn: false
   });
 
   console.log('Document written with ID: ', docRef.id);
 };
+
+const loadData = () => {
+  onSnapshot(collection(db, 'families'), (querySnapshot) => {
+    const families = [];
+
+    querySnapshot.forEach(doc => {
+      const family = {
+        id: doc.id,
+        name: doc.data().name,
+        drawn: doc.data().drawn
+      };
+
+      families.push(family);
+    });
+
+    familyNames.value = families;
+  });
+};
+
+onMounted(() => {
+  loadData();
+});
 </script>
 
 <template>
@@ -28,7 +63,7 @@ const handleAdd = async () => {
 
     <div class="flex justify-start-items-center gap-3 my-4">
       <TextInput
-        v-model="familyName"
+        v-model="family"
         placeholder="Nome da família"
         type="text"
       />
@@ -40,5 +75,7 @@ const handleAdd = async () => {
         @click="handleAdd"
       />
     </div>
+
+    <List :list="familyNames" />
   </div>
 </template>
