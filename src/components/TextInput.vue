65912750a1ed69036f2a65a1b8d7f484584e073d
David Freitas <davidfreitas@iMac-Pro-de-David.local>
2023-08-02 09:33:16 -0300

    criacao de componente de input

---
+++ src/components/TextInput.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { PhUser, PhLockSimple, PhEnvelopeSimple } from '@phosphor-icons/vue';
+
+const props = defineProps({
+  type: { 
+    type: String, 
+    default: '' 
+  },
+  icon: { 
+    type: String, 
+    default: '' 
+  },
+  text: { 
+    type: String, 
+    default: '' 
+  },
+  modelValue: { 
+    type: [ String, Number ], 
+    default: '' 
+  }
+});
+
+const emit = defineEmits(['update:modelValue', 'onKeyupEnter']);
+
+const updateValue = (event) => {
+  emit('update:modelValue', event.target.value);
+};
+</script>
+
+<template>
+  <div class="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-brand-hover">
+    <ph-user
+      v-if="icon === 'UserIcon'"
+      :size="32"
+    />
+
+    <ph-envelope-simple
+      v-if="icon === 'EnvelopeIcon'"
+      :size="32"
+    />
+
+    <ph-lock-simple
+      v-if="icon === 'LockClosedIcon'"
+      :size="32"
+    />
+
+    <input
+      className="bg-transparent flex-1 text-gray-100 text-base placeholder:text-gray-400 outline-none"
+      :type="type"
+      :placeholder="text"
+      :value="modelValue"
+      @input="updateValue"
+      @keyup.enter="$emit('onKeyupEnter')"
+    >
+  </div>
+</template>
